331a308f40cffe22d0c8c0fbd571015a8fc8d5ea
Maximiliano <maximilianokerps@gmail.com>
2022-09-24 03:05:07 -0300

    Corrección de sangrado

--- src/Controller/ProductoController.php
+++ src/Controller/ProductoController.php
@@ -2,9 +2,9 @@
     namespace App\Controller;
 
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-    use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
-    
+    use Symfony\Component\HttpFoundation\Response;
+
     use App\Manager\ProductoManager;
 
     class ProductoController extends AbstractController{
