0739a3a97f590991f02ef7592f43e497263002b4
Maximiliano <maximilianokerps@gmail.com>
2022-09-24 03:01:12 -0300

    new equals. new getTotal.

--- src/Entity/Item.php
+++ src/Entity/Item.php
@@ -1,77 +1,75 @@
 <?php
+    namespace App\Entity;
 
-namespace App\Entity;
+    use Doctrine\ORM\Mapping as ORM;
 
-use App\Repository\ItemRepository;
-use Doctrine\ORM\Mapping as ORM;
-
-/**
- * @ORM\Entity(repositoryClass=ItemRepository::class)
- */
-class Item
-{
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+    use App\Repository\ItemRepository;
 
     /**
-     * @ORM\Column(type="integer")
+     * @ORM\Entity(repositoryClass=ItemRepository::class)
      */
-    private $cantidad;
+    class Item{
+        /**
+         * @ORM\Id
+         * @ORM\GeneratedValue
+         * @ORM\Column(type="integer")
+         */
+        private $id;
 
-    /**
-     * @ORM\ManyToOne(targetEntity=Producto::class, inversedBy="items")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $producto;
+        /**
+         * @ORM\Column(type="integer")
+         */
+        private $cantidad;
 
-    /**
-     * @ORM\ManyToOne(targetEntity=Carrito::class, inversedBy="items")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $carrito;
+        /**
+         * @ORM\ManyToOne(targetEntity=Producto::class, inversedBy="items")
+         * @ORM\JoinColumn(nullable=false)
+         */
+        private $producto;
 
-    public function getId(): ?int
-    {
-        return $this->id;
+        /**
+         * @ORM\ManyToOne(targetEntity=Carrito::class, inversedBy="items")
+         * @ORM\JoinColumn(nullable=false)
+         */
+        private $carrito;
+
+        public function getId(): ?int {
+            return $this->id;
+        }
+
+        public function getCantidad(): ?int {
+            return $this->cantidad;
+        }
+
+        public function setCantidad(int $cantidad): self {
+            $this->cantidad = $cantidad;
+            return $this;
+        }
+
+        public function getProducto(): ?Producto {
+            return $this->producto;
+        }
+
+        public function setProducto(?Producto $producto): self {
+            $this->producto = $producto;
+            return $this;
+        }
+
+        public function getCarrito(): ?Carrito {
+            return $this->carrito;
+        }
+
+        public function setCarrito(?Carrito $carrito): self {
+            $this->carrito = $carrito;
+            return $this;
+        }
+
+        public function equals (Item $item): bool {
+            return $this->getProducto()->getId() === $item->getProducto()->getId();
+        }
+
+        public function getTotal(): int {
+            return $this->getCantidad() * $this->getProducto()->getPrecio();
+        }
     }
-
-    public function getCantidad(): ?int
-    {
-        return $this->cantidad;
-    }
-
-    public function setCantidad(int $cantidad): self
-    {
-        $this->cantidad = $cantidad;
-
-        return $this;
-    }
-
-    public function getProducto(): ?Producto
-    {
-        return $this->producto;
-    }
-
-    public function setProducto(?Producto $producto): self
-    {
-        $this->producto = $producto;
-
-        return $this;
-    }
-
-    public function getCarrito(): ?Carrito
-    {
-        return $this->carrito;
-    }
-
-    public function setCarrito(?Carrito $carrito): self
-    {
-        $this->carrito = $carrito;
-
-        return $this;
-    }
-}
+?>
