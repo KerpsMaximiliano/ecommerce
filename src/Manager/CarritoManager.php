4fc2ee5fe28a321d0db3f24edbff5288c5e0dde7
Maximiliano <maximilianokerps@gmail.com>
2022-09-24 03:05:51 -0300

    new functions

--- src/Manager/CarritoManager.php
+++ src/Manager/CarritoManager.php
@@ -1,43 +1,68 @@
 <?php
-    
     namespace App\Manager;
 
     use Doctrine\Persistence\ManagerRegistry;
+    use App\Entity\Producto;
+    use App\Entity\Usuario;
+    use App\Entity\Carrito;
+    use App\Entity\Item;
 
     class CarritoManager {
-        private $entityManager;
-
         public function __construct(ManagerRegistry $doctrine) {
-            $this->entityManager = $doctrine->getManager();
+            $this->doctrine = $doctrine;
         }
 
-        public function agregarProducto(
-            Usuario $usuario,
-            int $productoId,
-            int $cantidad
-        ) {
-            // ...
-            // doctrine === entityManager
-            $producto = $this->entityManager->getRepository(Producto::class)->find($productoId);
+        public function verCarrito(Usuario $usuario){
+            $estado = 'Iniciado';
+            return $carrito = $this->doctrine->getRepository(Carrito::class)->findOneBy(compact('usuario', 'estado'));
+        }
 
-            // Definición e iniciación de un objeto Carrito.
-            $carrito = new Carrito();
-
-            // Se configura dicho objeto.
-            $carrito->setEstado('Iniciado');
-            $carrito->setIniciado(new \DateTime());
-            $carrito->setUsuario($usuario);
-
-            // ...
+        public function agregarProducto(Usuario $usuario, int $productoId, int $cantidad) {
+            $producto = $this->doctrine->getRepository(Producto::class)->find($productoId);
+            $carrito = $this->obtenerCarrito($usuario);
             $item = $carrito->agregarItem($producto, $cantidad);
-
-            // Se persiste en la base de datos ...
-            // Objeto Item.
-            $entityManager->persist($item);
-            // Objeto Carrito.
+            $entityManager = $this->doctrine->getManager();
             $entityManager->persist($carrito);
-            // Se realiza ...
+            $entityManager->persist($item);
             $entityManager->flush();
+        }
+
+        public function finalizarCompra(Usuario $usuario){
+            $estado = 'Iniciado';
+            $carrito = $this->doctrine->getRepository(Carrito::class)->findOneBy(compact('usuario', 'estado'));
+            $carrito->setEstado('Finalizado');
+            $carrito->setConfirmado(new \DateTime());
+            $entityManager = $this->doctrine->getManager();
+            $entityManager->persist($carrito); // ?
+            $entityManager->flush();
+        }
+
+        public function eliminarItem(Usuario $usuario, int $itemId){
+            $item = $this->doctrine->getRepository(Item::class)->find($itemId);
+            $estado = 'Iniciado';
+            $carrito = $this->doctrine->getRepository(Carrito::class)->findOneBy(compact('usuario', 'estado'));
+            $carrito->removeItem($item);
+            $entityManager = $this->doctrine->getManager();
+            $entityManager->remove($item);
+            $entityManager->flush();
+        }
+
+        public function vaciarCarrito(Usuario $usuario){
+            $estado = 'Iniciado';
+            $carrito = $this->doctrine->getRepository(Carrito::class)->findOneBy(compact('usuario', 'estado'));
+            // Falta terminar.
+        }
+
+        private function obtenerCarrito($usuario){
+            $estado = 'Iniciado';
+            $carrito = $this->doctrine->getRepository(Carrito::class)->findOneBy(compact('usuario', 'estado'));
+            if(!$carrito){
+                $carrito = new Carrito();
+                $carrito->setEstado('Iniciado');
+                $carrito->setIniciado(new \DateTime());
+                $carrito->setUsuario($usuario);
+            }
+            return $carrito;
         }
     }
 ?>
