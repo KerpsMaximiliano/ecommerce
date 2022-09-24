a6fc49b0f2fb1796117c91a806f2fd43ad16d73e
Maximiliano <maximilianokerps@gmail.com>
2022-09-24 03:00:36 -0300

    modified addItem. new getTotal.

--- src/Entity/Carrito.php
+++ src/Entity/Carrito.php
@@ -1,142 +1,145 @@
 <?php
+    namespace App\Entity;
 
-namespace App\Entity;
+    use Doctrine\ORM\Mapping as ORM;
+    use Doctrine\Common\Collections\ArrayCollection;
+    use Doctrine\Common\Collections\Collection;
 
-use App\Repository\CarritoRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
-
-/**
- * @ORM\Entity(repositoryClass=CarritoRepository::class)
- */
-class Carrito
-{
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+    use App\Repository\CarritoRepository;
 
     /**
-     * @ORM\Column(type="string", length=50)
+     * @ORM\Entity(repositoryClass=CarritoRepository::class)
      */
-    private $estado;
+    class Carrito{
+        /**
+         * @ORM\Id
+         * @ORM\GeneratedValue
+         * @ORM\Column(type="integer")
+         */
+        private $id;
 
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $iniciado;
+        /**
+         * @ORM\Column(type="string", length=50)
+         */
+        private $estado;
 
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $confirmado;
+        /**
+         * @ORM\Column(type="datetime")
+         */
+        private $iniciado;
 
-    /**
-     * @ORM\OneToMany(targetEntity=Item::class, mappedBy="carrito")
-     */
-    private $items;
+        /**
+         * @ORM\Column(type="datetime", nullable=true)
+         */
+        private $confirmado;
 
-    /**
-     * @ORM\ManyToOne(targetEntity=Usuario::class, inversedBy="carritos")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $usuario;
+        /**
+         * @ORM\OneToMany(targetEntity=Item::class, mappedBy="carrito")
+         */
+        private $items;
 
-    public function __construct()
-    {
-        $this->items = new ArrayCollection();
-    }
+        /**
+         * @ORM\ManyToOne(targetEntity=Usuario::class, inversedBy="carritos")
+         * @ORM\JoinColumn(nullable=false)
+         */
+        private $usuario;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+        public function __construct(){
+            $this->items = new ArrayCollection();
+        }
 
-    public function getEstado(): ?string
-    {
-        return $this->estado;
-    }
+        public function getId(): ?int {
+            return $this->id;
+        }
 
-    public function setEstado(string $estado): self
-    {
-        $this->estado = $estado;
+        public function getEstado(): ?string {
+            return $this->estado;
+        }
 
-        return $this;
-    }
+        public function setEstado(string $estado): self {
+            $this->estado = $estado;
+            return $this;
+        }
 
-    public function getIniciado(): ?\DateTimeInterface
-    {
-        return $this->iniciado;
-    }
+        public function getIniciado(): ?\DateTimeInterface {
+            return $this->iniciado;
+        }
 
-    public function setIniciado(\DateTimeInterface $iniciado): self
-    {
-        $this->iniciado = $iniciado;
+        public function setIniciado(\DateTimeInterface $iniciado): self {
+            $this->iniciado = $iniciado;
+            return $this;
+        }
 
-        return $this;
-    }
+        public function getConfirmado(): ?\DateTimeInterface {
+            return $this->confirmado;
+        }
 
-    public function getConfirmado(): ?\DateTimeInterface
-    {
-        return $this->confirmado;
-    }
+        public function setConfirmado(\DateTimeInterface $confirmado): self {
+            $this->confirmado = $confirmado;
+            return $this;
+        }
 
-    public function setConfirmado(\DateTimeInterface $confirmado): self
-    {
-        $this->confirmado = $confirmado;
+        /**
+         * @return Collection<int, Item>
+         */
+        public function getItems(): Collection {
+            return $this->items;
+        }
 
-        return $this;
-    }
+        public function removeItem(Item $item): self {
+            if ($this->items->removeElement($item)) {
+                // set the owning side to null (unless already changed)
+                if ($item->getCarrito() === $this) {
+                    $item->setCarrito(null);
+                }
+            }
+            return $this;
+        }
 
-    /**
-     * @return Collection<int, Item>
-     */
-    public function getItems(): Collection
-    {
-        return $this->items;
-    }
+        public function getUsuario(): ?Usuario {
+            return $this->usuario;
+        }
 
-    public function addItem(Item $item): self
-    {
-        if (!$this->items->contains($item)) {
+        public function setUsuario(?Usuario $usuario): self {
+            $this->usuario = $usuario;
+
+            return $this;
+        }
+
+        public function agregarItem(Producto $producto, int $cantidad): Item {
+            $item = new Item();
+            $item->setProducto($producto);
+            $item->setCantidad($cantidad);
+            return $this->addItem($item);
+        }
+
+//      public function addItem(Item $item): self {
+//          if (!$this->items->contains($item)) {
+//              $this->items[] = $item;
+//              $item->setCarrito($this);
+//          }
+//          return $this;
+//      }
+
+        // new function addItem
+        public function addItem(Item $item): Item {
+            foreach($this->getItems() as $itemExistente) {
+                if($item->equals($itemExistente)){
+                    $itemExistente->setCantidad($item->getCantidad());
+                    return $itemExistente;
+                }
+            }
             $this->items[] = $item;
             $item->setCarrito($this);
+            return $item;
         }
 
-        return $this;
-    }
-
-    public function agregarItem($producto, $cantidad){
-        $item = new Item();
-        $item->setProducto($producto);
-        $item->setCantidad($cantidad);
-        addItem($item);
-    }
-
-    public function removeItem(Item $item): self
-    {
-        if ($this->items->removeElement($item)) {
-            // set the owning side to null (unless already changed)
-            if ($item->getCarrito() === $this) {
-                $item->setCarrito(null);
+        public function getTotal(){
+            $total = 0;
+            foreach($this->getItems() as $item){
+                $total += $item->getTotal();
             }
+            return $total;
         }
-
-        return $this;
     }
-
-    public function getUsuario(): ?Usuario
-    {
-        return $this->usuario;
-    }
-
-    public function setUsuario(?Usuario $usuario): self
-    {
-        $this->usuario = $usuario;
-
-        return $this;
-    }
-}
+?>
